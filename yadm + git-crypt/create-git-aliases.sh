72b82d17189939e03f9b94e561550c0ec0de4dd2
crazy-matt <46377983+crazy-matt@users.noreply.github.com>
2021-02-14 18:28:56 +0000

    add an alias for yadm updates

--- yadm + git-crypt/create-git-aliases.sh
+++ yadm + git-crypt/create-git-aliases.sh
@@ -5,3 +5,4 @@ git config --global alias.decipher '!f() { zsh -c ". ${ZDOTDIR:-$HOME}/.zaliases
 git config --global alias.ls '!f() { zsh -c ". ${ZDOTDIR:-$HOME}/.zaliases; yadm_ls \"$1\" "; }; f'
 git config --global alias.unlock '!f() { zsh -c ". ${ZDOTDIR:-$HOME}/.zaliases; yadm_unlock \"$1\" "; }; f'
 git config --global alias.open '!f() { open "$(yadm remote get-url origin)"; }; f'
+git config --global alias.update '!f() { zsh -c "echo \"$(crontab -l)\" > ~/.crontab && yadm add ~/.crontab && yadm add -u && yadm commit -m \"$1\" && yadm push;"; }; f'
